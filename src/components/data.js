f38fc55daaeacb1ad5bb2d70d6b6ae93cf23051a
Senadhian2003 <senadhian@gmail.com>
2022-11-10 22:13:21 +0530

    sena retrieve commit

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,4 @@
-let data = [{name : 'Python Coding',btnId : 'pythonCoding'},
-{name : 'Paper Presentation',btnId : 'paperPresentation'}]
+let data = [{name : 'Python Coding',btnId : 'pythonCoding',dbName:'python-coding'},
+{name : 'Paper Presentation',btnId : 'paperPresentation',dbName : 'paper-presentation'}]
 
 export default data
