06ebab4391b0e3f34e4f334679df56984fdeba38
Senadhian2003 <senadhian@gmail.com>
2022-11-11 10:43:21 +0530

    routes declared

--- src/components/certificate-winner.jsx
+++ src/components/certificate-winner.jsx
@@ -1,10 +1,177 @@
 import React from 'react'
 import Navbar1 from './navbar1';
+import { useState, useEffect } from 'react';
+import  "../firebase.js"
+import { getDatabase, ref, child, get } from "firebase/database";
 
 export default function CertificateWinner() {
-  return (
+
+    const [data, setData] = useState();
+    const [name,setName] = useState();
+    const [option,setOption] = useState('python-coding');
+
+    useEffect(() => {
+        console.log('trigger use effect hook');
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `winner/${option}`)).then((snapshot) => {
+          if (snapshot.exists()) {
+  
+          //   console.log(snapshot.val());
+              let x = snapshot.val();
+              let arr =Object.values(x)
+            //   console.log(arr)
+              setData(arr);
+              let arrName = []
+              for(let i=0;i<3;i++){
+                get(child(dbRef, `users/${arr[i]}`)).then((snapshot) => {
+                  if (snapshot.exists()) {
+          
+                  //   console.log(snapshot.val());
+                      let x = snapshot.val();
+                      let arr1 =Object.values(x)
+                      let obj = {
+                        id : arr[i],
+                        name : arr1[2]
+                      }
+                      arrName.push(obj)
+                      
+                      
+                   
+                  
+                  } else {
+                    console.log("No data available");
+                    alert("No data available")
+                  }
+                }).catch((error) => {
+                  console.error(error);
+                });
+              }
+              setName(arrName)
+              
+           
+          
+          } else {
+            console.log("No data available");
+          }
+        }).catch((error) => {
+          console.error(error);
+        });
+        
+       
+      },[option])
+
+
+      const getName = ()=>{
+        // console.log(data)
+        console.log(name)
+      }
+
+      // const getName = ()=>{
+      //   console.log(data)
+      //   console.log(option)
+
+      //   console.log('trigger User Click');
+      //   const dbRef = ref(getDatabase());
+      //   let arrName = []
+      //   for(let i=0;i<3;i++){
+      //   get(child(dbRef, `users/${data[i]}`)).then((snapshot) => {
+      //     if (snapshot.exists()) {
+  
+      //     //   console.log(snapshot.val());
+      //         let x = snapshot.val();
+      //         let arr =Object.values(x)
+      //         arrName.push(arr[2])
+              
+              
+           
+          
+      //     } else {
+      //       console.log("No data available");
+      //       alert("No data available")
+      //     }
+      //   }).catch((error) => {
+      //     console.error(error);
+      //   });
+      // }
+      
+      // setName(arrName);
+
+
+      // }
+
+      // useEffect(() => {
+      //   console.log('trigger use effect hook');
+      //   const dbRef = ref(getDatabase());
+      //   for(let i=0;i<3;i++){
+      //   get(child(dbRef, `users/${data[i]}`)).then((snapshot) => {
+      //     if (snapshot.exists()) {
+  
+      //     //   console.log(snapshot.val());
+      //         let x = snapshot.val();
+      //         let arrName =Object.values(x)
+      //         console.log(arrName)
+      //         // setData(arr);
+              
+           
+          
+      //     } else {
+      //       console.log("No data available");
+      //     }
+      //   }).catch((error) => {
+      //     console.error(error);
+      //   });
+      // }
+       
+      // },[data])
+
+      if (data) return  (
     <div>
         <Navbar1/>
+        <div className="container" style={{marginTop : '10%'}}>
+        <select class="form-select" onChange={(e)=>{
+            setOption(e.target.value)
+         
+        }} aria-label="Default select example">
+ 
+ <option value="python-coding">Python Coding</option>
+ <option value="paper-presentation">Paper Presentation</option>
+ <option value="3">Good</option>
+</select>
+   
+
+        <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Place</th>
+      <th scope="col">Cyber ID</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>First</td>
+      <td>Otto</td>
+      
+    </tr>
+    <tr>
+     
+      <td>Second</td>
+      <td>Thornton</td>
+      
+    </tr>
+    <tr>
+  
+      <td>Third</td>
+      <td>the Bird</td>
+      
+    </tr>
+  </tbody>
+</table>
+
+<button onClick={getName} >Click Me</button>
+
+</div>
     </div>
   )
 }
--- src/components/certificateParticipants.jsx
+++ src/components/certificateParticipants.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {db} from "../firebase.js"
+import { getDatabase,update, ref, child, get } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+import "./Cert.css"
 import Navbar1 from './navbar1';
-
-
 export default function CertificateParticipants() {
-  return (
-    <div>
-        <Navbar1/>
+  const [data,setdata]=useState()
+  const [allow,setallow]=useState()
+const dbRef = ref(getDatabase());
+const navigate = useNavigate();
+useEffect(() => {
+  get(child(dbRef, 'users')).then((snapshot) => {
+  
+    if (snapshot.exists()) {
+      let x=snapshot.val()
+      let a = Object.values(x);
+      setdata(a)
+      setallow()
+
+    
+     
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+}, [allow]) 
+function Suc(e){
+  let id=e.target.id
+  console.log(id)
+  update(ref(db,`/users/${id}`),{
+    isParticipate:2
+   }) 
+   setallow("hi")
+   
+
+
+  console.log(e.target.id)
+}
+function fail(e){
+  let id=e.target.id
+  console.log(id)
+  update(ref(db,`/users/${id}`),{
+    isParticipate:1
+   }) 
+   setallow("hi")
+
+}
+
+if(data) return (
+  <div>
+     <Navbar1/>
+    <table>
+      
+      {
+       (data.map((data)=>{
+        if(data.isParticipate>=1) return <thead className={data.isParticipate == 2 ? "act":"norm"}>
+        <tr >
+          <td>{data.Name}</td>
+          <td>{data.PythonCoding=== 1 ? "YES":"NO"}</td>
+          <td>{data.SoloCompile=== 1 ? "YES":"NO"}</td>
+          <td>{data.PapperPresentation=== 1 ? "YES":"NO"}</td>
+          <td>{data.OptimizeCoding=== 1 ? "YES":"NO"}</td>
+          <td>{data.CaptureTheFlag=== 1 ? "YES":"NO"}</td>
+          <td> <button id={data.UID}  onClick={Suc}>Given</button></td>
+          <td><button id={data.UID} onClick={fail} >Not Given</button></td>
+          {console.log(data.isParticipate)}
+
+         
+         </tr>
+         </thead>
+          
+          
+       
+        }))
+      }
+    </table>
+
     </div>
   )
 }
